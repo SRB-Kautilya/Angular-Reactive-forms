bcd60502dd8ef796c82279e1fe4ecfc42e4a9be2
Shashank reddy Bobbala <shashankreddybobbala@Shashanks-MacBook-Pro.local>
2019-10-05 17:14:23 -0400

    git commit -m "created aform in the code"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
 
   //The first step is importing form group from '@angular/forms
   signupForm:FormGroup
+
+
+ngOnInit(){
+  //As it is type of form we have to create new formGroup which takes js object
+  this.signupForm = new FormGroup({
+    //in this js object we have to add controlls(ehich are key value pairs of formGroup) 
+    //FormControl another class takes 3 args imported from @angular/forms. The 3 args are initial state, single validator or array of validators, async validators
+    'username': new FormControl(null),
+    'email':new FormControl(null),
+    'gender':new FormControl('male')
+  })
+  }
 }
+
